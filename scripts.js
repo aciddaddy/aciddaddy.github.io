314e64e1787ed22b4ef86997ca2bd32f685b53c0
aciddaddy <theaciddaddy@gmail.com>
2024-09-17 17:17:03 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,1 @@
-// Function to open the lightbox with the clicked image
-function openLightbox(imageSrc) {
-    const lightbox = document.getElementById('lightbox');
-    const lightboxImage = document.getElementById('lightbox-image');
-    lightbox.style.display = 'block'; // Show lightbox
-    lightboxImage.src = imageSrc; // Set the source of the lightbox image
-}
-
-// Function to close the lightbox
-function closeLightbox() {
-    const lightbox = document.getElementById('lightbox');
-    lightbox.style.display = 'none'; // Hide lightbox
-}
 
